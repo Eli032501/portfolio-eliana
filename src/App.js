9fe46dc2b5ed7164fdedac0cbd3c522afb1b57a6
Eliana Pereira <98112983+Eli032501@users.noreply.github.com>
2024-06-28 23:43:14 +0100

    create new comp-projects

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Navheader from "./Navheader.js";
 import Header from "./Header.js";
+import MyProjects from "./MyProjects.js";
 
 function App() {
   return (
     <div className="App container-fluid">
       <Navheader />
       <Header />
+      <MyProjects />
     </div>
   );
 }
---
+++ src/MyProjects.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./MyProjects.css";
+
+export default function MyProjects(params) {
+  return (
+    <div className="MyProjects">
+      <div>
+        <h2 className="title-style">My Projects</h2>
+        <span className="title-component-rt"></span>
+      </div>
+    </div>
+  );
+}
