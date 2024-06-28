0aca225ce15f98129faa3db25faab325ea26c818
Eliana Pereira <98112983+Eli032501@users.noreply.github.com>
2024-06-28 12:39:39 +0100

    create files and added navbar-bootstrap

---
+++ src/Header.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div className="Header">
+      <section>
+        <h1>Frontend Developer</h1>
+        <strong> Based in Lisbon</strong>
+        <div>
+          <h3>Skills</h3>
+          <ul>
+            <li>React</li>
+            <li>Bootstrap</li>
+            <li>Responsive Developement</li>
+          </ul>
+        </div>
+        <div>
+          <h3>Tools</h3>
+          <ul>
+            <li>Github</li>
+            <li>Vs Code</li>
+          </ul>
+        </div>
+      </section>
+      <section>
+        <image />
+      </section>
+    </div>
+  );
+}
---
+++ src/Navheader.js
@@ -0,0 +1,29 @@
+import { Navbar, Nav, Container, Offcanvas } from "react-bootstrap";
+import "./Navheader.css";
+
+export default function Navheader() {
+  return (
+    <div className="Navheader">
+      <Navbar expand={false} className="bg-body-tertiary mb-3">
+        <Container fluid>
+          <Navbar.Brand href="#">Eliana Pereira</Navbar.Brand>
+          <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${false}`} />
+          <Navbar.Offcanvas
+            id={`offcanvasNavbar-expand-${false}`}
+            aria-labelledby={`offcanvasNavbarLabel-expand-${false}`}
+            placement="end"
+          >
+            <Offcanvas.Header closeButton></Offcanvas.Header>
+            <Offcanvas.Body>
+              <Nav className="justify-content-end flex-grow-1 pe-3">
+                <Nav.Link href="#action1">My Projects</Nav.Link>
+                <Nav.Link href="#action2">About Me</Nav.Link>
+                <Nav.Link href="#action2">Contact Me</Nav.Link>
+              </Nav>
+            </Offcanvas.Body>
+          </Navbar.Offcanvas>
+        </Container>
+      </Navbar>
+    </div>
+  );
+}
