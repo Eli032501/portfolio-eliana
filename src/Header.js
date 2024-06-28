d25f74e90d98d4607c367a3d3ae09653be300a7a
Eliana Pereira <98112983+Eli032501@users.noreply.github.com>
2024-06-28 23:32:25 +0100

    header button

--- src/Header.js
+++ src/Header.js
@@ -7,31 +7,29 @@ export default function Header() {
     <div className="Header">
       <section className="row">
         <div className=" col-7">
-          <div className=" frame-header">
-            <div className="container-hd">
-              <h1>Frontend Developer</h1>
-              <p> Based in Lisbon</p>
-              <div className="row mt-5">
-                <div className="col">
-                  <strong>Skills</strong>
-                  <ul>
-                    <li>React</li>
-                    <li>Bootstrap</li>
-                    <li>Responsive Developement</li>
-                  </ul>
-                </div>
-                <div className="col-1"></div>
-                <div className="col">
-                  <strong>Tools</strong>
-                  <ul>
-                    <li>Github</li>
-                    <li>Vs Code</li>
-                  </ul>
-                </div>
+          <div className=" frame-header mb-5">
+            <h1>Frontend Developer</h1>
+            <p> Based in Lisbon</p>
+            <div className="row mt-5">
+              <div className="col">
+                <strong>Skills</strong>
+                <ul>
+                  <li>React</li>
+                  <li>Bootstrap</li>
+                  <li>Responsive Developement</li>
+                </ul>
+              </div>
+              <div className="col-1"></div>
+              <div className="col">
+                <strong>Tools</strong>
+                <ul>
+                  <li>Github</li>
+                  <li>Vs Code</li>
+                </ul>
               </div>
             </div>
           </div>
-          <button className="btn">Contact me</button>
+          <button className="btn btn-standard-lt">Contact me</button>
         </div>
         <div className="frame-image col-5">
           <img src={perfilImage} alt="profile" />
