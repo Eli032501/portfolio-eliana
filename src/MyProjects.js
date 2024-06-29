ce2b75d22c91fd462f079131292f9afc62aa098f
Eliana Pereira <98112983+Eli032501@users.noreply.github.com>
2024-06-29 12:18:23 +0100

    style - projects elements

--- src/MyProjects.js
+++ src/MyProjects.js
@@ -1,12 +1,21 @@
 import React from "react";
 import "./MyProjects.css";
+import prj1 from "./images/prj1.png";
 
 export default function MyProjects(params) {
   return (
     <div className="MyProjects">
-      <div>
+      <div className="d-flex f-row justify-content-end align-items-center">
         <h2 className="title-style">My Projects</h2>
         <span className="title-component-rt"></span>
+      </div>
+      <div className="grid-projects">
+        <div className="element-project">
+          <div>
+            <img src={prj1} alt="weather project" />
+          </div>
+          <h3>Check the Weather</h3>
+        </div>
       </div>
     </div>
   );
