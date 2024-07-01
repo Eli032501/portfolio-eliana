1b1b6b0bd7153318455e4137338c817ee652a142
Eliana Pereira <98112983+Eli032501@users.noreply.github.com>
2024-07-01 13:35:57 +0100

    toggle prj element and styling

--- src/MyProjects.js
+++ src/MyProjects.js
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./MyProjects.css";
 import prj1 from "./images/prj1.png";
 
 export default function MyProjects(params) {
+  const [btnToggle, setBtnToggle] = useState("d-none");
+
+  function togglePrj() {
+    if (btnToggle === "d-none") {
+      setBtnToggle("d-block");
+    } else {
+      setBtnToggle("d-none");
+    }
+  }
+
   return (
     <div className="MyProjects">
       <div className="d-flex f-row justify-content-end align-items-center">
@@ -10,12 +20,28 @@ export default function MyProjects(params) {
         <span className="title-component-rt"></span>
       </div>
       <div className="grid-projects">
-        <div className="element-project">
-          <div>
+        <button onClick={togglePrj} className="element-project">
+          <div className="frame-prj">
             <img src={prj1} alt="weather project" />
           </div>
+          <div className={[btnToggle, "info-prj"].join(" ")}>
+            <p>
+              I built this fully-functioning Vanilla JavaScript weather app in
+              SheCodes workshop. Building upon my previously-gained knowledge, I
+              also became skilled in Bootstrap, API’s, real-life development
+              workflow, hosting, and advanced JavaScript.
+            </p>
+            <a
+              href="https://react-weather-forecast-eli.netlify.app/"
+              className="link-prj"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Check it out!
+            </a>
+          </div>
           <h3>Check the Weather</h3>
-        </div>
+        </button>
       </div>
     </div>
   );
