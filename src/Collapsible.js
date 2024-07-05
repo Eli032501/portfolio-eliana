fa7bd5c28e7537b2baf203f814cf8956d02591cd
Eliana Pereira <98112983+Eli032501@users.noreply.github.com>
2024-07-05 18:35:09 +0100

    responsive md projects

--- src/Collapsible.js
+++ src/Collapsible.js
@@ -3,26 +3,43 @@ import React, { useState } from "react";
 export default function Collapsible({ label, body, link }) {
   const [open, setOPen] = useState(false);
 
-  const toggle = () => {
-    setOPen(!open);
-  };
-  return (
-    <div>
-      {open && (
-        <div className="info-prj ">
-          <p>{body}</p>
-          <a
-            href="https://react-weather-forecast-eli.netlify.app/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            {link}
-          </a>
-        </div>
-      )}
-      <button onClick={toggle}>
+  if (window.innerWidth >= 992) {
+    const toggle = () => {
+      setOPen(!open);
+    };
+    return (
+      <div>
+        <button onClick={toggle}>
+          <h3>{label}</h3>
+        </button>
+        {open && (
+          <div className="info-prj ">
+            <p>{body}</p>
+            <a
+              href="https://react-weather-forecast-eli.netlify.app/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {link}
+            </a>
+          </div>
+        )}
+      </div>
+    );
+  } else {
+    return (
+      <div className="info-prj ">
         <h3>{label}</h3>
-      </button>
-    </div>
-  );
+
+        <p>{body}</p>
+        <a
+          href="https://react-weather-forecast-eli.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {link}
+        </a>
+      </div>
+    );
+  }
 }
