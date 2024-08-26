7dada8dba8f392a6b598e6d6f2df4dc88999ef3e
Eliana Pereira <98112983+Eli032501@users.noreply.github.com>
2024-08-26 20:25:13 +0100

    changes in header - color and font

--- src/Header.js
+++ src/Header.js
@@ -5,39 +5,36 @@ import "./Header.css";
 export default function Header() {
   return (
     <div className="Header">
-      <section className="row">
-        <div className="d-none d-sm-block col d-md-none "></div>
-        <div className="  col-12 col-sm-11 col-md-7">
-          <div className=" frame-header ">
-            <h1>Frontend Developer</h1>
-            <p> based in Lisbon</p>
-            <div className="row grp-lists">
-              <div className="col ">
-                <strong>Skills</strong>
-                <ul>
-                  <li>React.js</li>
-                  <li>Bootstrap</li>
-                  <li>Responsive</li>
-                </ul>
-              </div>
+      <section className="header-grid">
+        <div className=" frame-header">
+          <h1>Frontend Developer</h1>
+          <p> based in Lisbon</p>
+          <div className="row grp-lists">
+            <div className="col ">
+              <strong>Skills</strong>
+              <ul>
+                <li>React.js</li>
+                <li>Bootstrap</li>
+                <li>Responsive</li>
+              </ul>
+            </div>
 
-              <div className="col    ">
-                <strong>Tools</strong>
-                <ul>
-                  <li>Github</li>
-                  <li>Vs Code</li>
-                </ul>
-              </div>
+            <div className="col    ">
+              <strong>Tools</strong>
+              <ul>
+                <li>Github</li>
+                <li>Vs Code</li>
+              </ul>
             </div>
           </div>
           <a href="#contact" className="d-none d-md-block btn btn-standard-lt">
             Contact me
           </a>
         </div>
-        <div className="frame-image col-12 col-sm-10 col-md-5">
+
+        <div className="frame-image ">
           <img src={perfilImage} alt="profile" />
         </div>
-        <div className="d-none d-sm-block col d-md-none "></div>
       </section>
     </div>
   );
